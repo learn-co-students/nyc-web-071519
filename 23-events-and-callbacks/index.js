661c440231ce21b91ded59c1a45f751abc1ded3e
sbal13 <sbal13@gmail.com>
2019-08-28 11:20:14 -0400

    done

---
+++ 23-events-and-callbacks/index.js
@@ -0,0 +1,48 @@
+// const data = [
+//   {
+//     id: 1,
+//     name: "Settlers of Catan",
+//     description: "Friendships will be tested.",
+//     img_url: "https://crowscastle.com/375-large_default/the-settlers-of-catan.jpg"
+//   },
+//   {
+//     id: 2,
+//     name: "Monopoly",
+//     description: "Friendships will end.",
+//     img_url: "https://i5.walmartimages.com/asr/e9009e67-fbb0-4e96-83c5-5af0eeb13b28_1.f17117ff29f8aa4992cb93bad9205dc4.jpeg"
+//   },
+//   {
+//     id: 3,
+//     name: "Azul",
+//     description: "Great fun with the friends!.",
+//     img_url: "https://cf.geekdo-images.com/itemrep/img/ql-0-t271LVGqbmWA1gdkIH7WvM=/fit-in/246x300/pic3718275.jpg"
+//   }
+// ]
+
+// const container = document.querySelector("#container")
+
+// data.forEach(function (el, I, arr){
+//   const str = `<div class="tile">
+//             <h4>${el.name}</h4>
+//       <p>${el.description}</p>
+//             <div class="image-wrapper">
+//               <img src="${el.img_url}" alt="${el.name}">
+//             </div>
+//              </div>`
+
+//   container.insertAdjacentHTML("beforeend", str)
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
