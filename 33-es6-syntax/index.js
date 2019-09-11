0a794adbc44a43d65faec0700dad156d62335991
sbal13 <sbal13@gmail.com>
2019-09-11 10:13:56 -0400

    after es6

---
+++ 33-es6-syntax/index.js
@@ -0,0 +1,19 @@
+// let person = {
+//   name: "Beefy McSteaker",
+//   age: 15,
+//   specialSkill: "dancing",
+//   car: {make: "Tesla", model: "Model 3", year: 2019}
+// }
+// let { name, age, car: { make } } = person
+
+
+// let name = "Beefy"
+// let age = 25
+// let specialSkill = "dancing"
+// const person = { name, age, specialSkill, sup: "fam"}
+
+
+// let array = ["hello", "goodbye", "beef", "sup"]
+// let [ first, second, third ] = array
+
+
